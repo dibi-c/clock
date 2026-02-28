47a2879e2a38c9f097c65283f2a87ba39342206c
dibi-c <dyanbudi.db@gmail.com>
2026-02-28 09:42:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bms-remote-v6.5'; // Naikkan versi ini setiap kali Anda update kode di GitHub
+const CACHE_NAME = 'bms-remote-v6.6'; // Naikkan versi ini setiap kali Anda update kode di GitHub
 const assetsToCache = [
   './',
   './index.html',
